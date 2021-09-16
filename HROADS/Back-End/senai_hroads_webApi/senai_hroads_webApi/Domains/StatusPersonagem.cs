b179c73da902e9477d0ef3d059a138496406e87d
Leonardo Souza <leonardo030605@gmail.com>
2021-09-16 09:22:04 -0300

    Criação da base do projeto e configuração do Swagger e do Token

---
+++ HROADS/Back-End/senai_hroads_webApi/senai_hroads_webApi/Domains/StatusPersonagem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace senai_hroads_webApi.Domains
+{
+    public partial class StatusPersonagem
+    {
+        public int IdStatus { get; set; }
+        public int? IdHabilidade { get; set; }
+        public int? IdClasse { get; set; }
+
+        public virtual Classe IdClasseNavigation { get; set; }
+        public virtual Habilidade IdHabilidadeNavigation { get; set; }
+    }
+}
