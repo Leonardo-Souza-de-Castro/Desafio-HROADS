2f040c6a825b2ba7b4ba390016b2259f70cb9bf5
livianegrini <liviasnegrini2004@gmail.com>
2021-09-17 00:57:45 -0300

    CRUD TiposUsuarios e TiposHabilidades pronto, Usuarios faltand Login

---
+++ HROADS/Back-End/senai.hroads.webApi/senai.hroads.webApi/Controllers/TiposHabilidadesController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using senai.hroads.webApi.Domains;
+using senai.hroads.webApi.Interfaces;
+using senai.hroads.webApi.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.hroads.webApi.Controllers
+{
+    [Produces("application/json")]
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class TiposHabilidadesController : ControllerBase
+    {
+        private ITipoHabilidadeRepository _TiposHabilidadesRepository { get; set; }
+
+        public TiposHabilidadesController()
+        {
+            _TiposHabilidadesRepository = new TipoHabilidadeRepository();
+        }
+
+        [HttpGet]
+        public IActionResult ListarTodos()
+        {
+            return Ok(_TiposHabilidadesRepository.ListarTodos());
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult BuscarPorId(int Id)
+        {
+            return Ok(_TiposHabilidadesRepository.BuscarPorId(Id));
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(TiposHabilidade NovoTipoHabilidade)
+        {
+            _TiposHabilidadesRepository.Cadastrar(NovoTipoHabilidade);
+
+            return Ok();
+        }
+
+        [HttpPut("{Id}")]
+        public IActionResult Atualizar(int Id, TiposHabilidade TipoHabilidadeAtualizado)
+        {
+            _TiposHabilidadesRepository.Atualizar(Id, TipoHabilidadeAtualizado);
+
+            return Ok();
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult Deletar(int Id)
+        {
+            _TiposHabilidadesRepository.Deletar(Id);
+
+            return Ok();
+        }
+    }
+}
---
+++ HROADS/Back-End/senai.hroads.webApi/senai.hroads.webApi/Repositories/TipoHabilidadeRepository.cs
@@ -0,0 +1,56 @@
+﻿using senai.hroads.webApi.Contexts;
+using senai.hroads.webApi.Domains;
+using senai.hroads.webApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.hroads.webApi.Repositories
+{
+    public class TipoHabilidadeRepository : ITipoHabilidadeRepository
+    {
+
+        HroadsContext Ctx = new HroadsContext();
+
+        public void Atualizar(int IdIdTipoHabilidade, TiposHabilidade TipoHabilidadeAtualizado)
+        {
+            TiposHabilidade TipoHabilidadeBuscado = BuscarPorId(IdIdTipoHabilidade);
+
+            if (TipoHabilidadeAtualizado.TipoHabilidade != null)
+            {
+                TipoHabilidadeBuscado.TipoHabilidade = TipoHabilidadeAtualizado.TipoHabilidade;
+            }
+
+            Ctx.TiposHabilidades.Update(TipoHabilidadeBuscado);
+
+            Ctx.SaveChanges();
+        }
+
+        public TiposHabilidade BuscarPorId(int IdTipoHabilidade)
+        {
+            return Ctx.TiposHabilidades.FirstOrDefault(e => e.IdTipo == IdTipoHabilidade);
+        }
+
+        public void Cadastrar(TiposHabilidade NovoTipoHabilidade)
+        {
+            Ctx.TiposHabilidades.Add(NovoTipoHabilidade);
+
+            Ctx.SaveChanges();
+        }
+
+        public void Deletar(int IdTipoHabilidade)
+        {
+            TiposHabilidade TipoHabilidadeBuscado = BuscarPorId(IdTipoHabilidade);
+
+            Ctx.TiposHabilidades.Remove(TipoHabilidadeBuscado);
+
+            Ctx.SaveChanges();
+        }
+
+        public List<TiposHabilidade> ListarTodos()
+        {
+            return Ctx.TiposHabilidades.ToList();
+        }
+    }
+}
